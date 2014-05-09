6f95472131cd6f371a3757d41d6c496dbb1483a1
Joe Hudson <joehud@gmail.com>
2014-05-09 09:28:09 -0400

    use 'React' in the tests

--- test/test.js
+++ test/test.js
@@ -7,46 +7,45 @@ var chai = require('chai'),
     mixin4 = {type: 4};
 
 describe('react-mixin-dependencies', function() {
-  var toTest, mixins;
+  var React;
   beforeEach(function() {
-    toTest = {};
-    reactMixinDependencies(toTest);
-    mixins = toTest.mixins;
+    React = {};
+    reactMixinDependencies(React);
   });
 
   it('should return standard mixins', function() {
-    var rtn = mixins.get(mixin1, mixin2);
+    var rtn = React.mixins.get(mixin1, mixin2);
     expect(rtn).to.eql([mixin1, mixin2]);
   });
 
   it('should return named mixins', function() {
-    mixins.add('1', mixin1);
-    rtn = mixins.get('1', mixin2);
+    React.mixins.add('1', mixin1);
+    rtn = React.mixins.get('1', mixin2);
     expect(rtn).to.eql([mixin1, mixin2]);
   });
 
   it('should return named mixins and dependencies', function() {
-    mixins.add('1', mixin1);
-    mixins.add('2', mixin2, '1');
-    rtn = mixins.get('2');
+    React.mixins.add('1', mixin1);
+    React.mixins.add('2', mixin2, '1');
+    rtn = React.mixins.get('2');
     expect(rtn).to.eql([mixin1, mixin2]);
   });
 
   it('should return named n-level dependencies and not duplicate', function() {
-    mixins.add('1', mixin1);
-    mixins.add('2', mixin2, '1');
-    mixins.add('3', mixin3, '1', '2');
-    mixins.add('4', mixin4, '1', '3');
-    rtn = mixins.get('4', '2');
+    React.mixins.add('1', mixin1);
+    React.mixins.add('2', mixin2, '1');
+    React.mixins.add('3', mixin3, '1', '2');
+    React.mixins.add('4', mixin4, '1', '3');
+    rtn = React.mixins.get('4', '2');
     expect(rtn).to.eql([mixin1, mixin2, mixin3, mixin4]);
   });
 
   it('should extract list arguments', function() {
-    mixins.add('1', mixin1);
-    mixins.add('2', mixin2);
-    mixins.add('3', mixin3);
-    mixins.add('4', mixin4);
-    rtn = mixins.get('1', ['2', '3']);
+    React.mixins.add('1', mixin1);
+    React.mixins.add('2', mixin2);
+    React.mixins.add('3', mixin3);
+    React.mixins.add('4', mixin4);
+    rtn = React.mixins.get('1', ['2', '3']);
     expect(rtn).to.eql([mixin1, mixin2, mixin3]);
   });
 
