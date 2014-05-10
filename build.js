2d6111280f597663786c8495094feef0559e8462
Joe Hudson <joehud@gmail.com>
2014-05-10 16:01:35 -0400

    add index.js

--- build.js
+++ build.js
@@ -1,8 +1,18 @@
 var fs = require('fs'),
     UglifyJS = require('uglify-js');
 
-var version = JSON.parse(fs.readFileSync('./package.json', {encoding: 'utf-8'})).version;
+var packageInfo = JSON.parse(fs.readFileSync('./package.json', {encoding: 'utf-8'})),
+    name = packageInfo.name,
+    version = packageInfo.version,
+    file = './' + name + '.js',
+    minimizedFile = './' + name + '.min.js',
+    repo = 'https://github.com/jhudson8/' + name,
+    content = fs.readFileSync(file, {encoding: 'utf8'}),
+    versionMatcher = new RegExp(name + ' v[0-9\.]+');
 
-var minimized = UglifyJS.minify('./react-mixin-manager.js');
-var minimizedHeader = '/*!\n * react-mixin-manager v' + version + ';  MIT license\n */\n';
-fs.writeFileSync('./react-mixin-manager.min.js', minimizedHeader + minimized.code, {encoding: 'utf8'});
+content = content.replace(versionMatcher, name + ' v' + version);
+fs.writeFileSync(file, content, {encoding: 'utf8'});
+
+var minimized = UglifyJS.minify(file);
+var minimizedHeader = '/*!\n * [' + name + '](' + repo + ') v' + version + ';  MIT license; Joe Hudson<joehud@gmail.com>\n */\n';
+fs.writeFileSync(minimizedFile, minimizedHeader + minimized.code, {encoding: 'utf8'});
